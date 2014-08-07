4ee72681834020a93b05e3db1f03504dc67ec515
Denis B <den_at_okob_net>
2014-08-07 00:09:34 -0400

    Bumped up version to 1.1.1

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -45,7 +45,7 @@
 #include <SPI.h>
 #endif
 
-#define VERSION_STRING  "1.1.0 X2 Beta 1"
+#define VERSION_STRING  "1.1.1 X2 Beta 1"
 
 // look here for descriptions of gcodes: http://linuxcnc.org/handbook/gcode/g-code.html
 // http://objects.reprap.org/wiki/Mendel_User_Manual:_RepRapGCodes
